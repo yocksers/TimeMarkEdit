6435ceb33edb1d10f20c93f1ff35f48e97c8a7d2
yock <167619555+yocksers@users.noreply.github.com>
2026-03-28 23:34:09 +0100

    Add files via upload

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("d5e8c4b9-3f6a-4e2b-9c7d-0e1f4b8a3c62")]
 
-[assembly: AssemblyVersion("0.7.2.0")]
-[assembly: AssemblyFileVersion("0.7.2.0")]
+[assembly: AssemblyVersion("0.7.3.0")]
+[assembly: AssemblyFileVersion("0.7.3.0")]
